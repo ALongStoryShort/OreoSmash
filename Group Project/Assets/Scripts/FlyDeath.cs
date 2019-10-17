466af25610cbc5652d1d43aef5fb16abe40fb443
K-auiwfhwegjku <kaijie.zhou@digipen.edu>
2019-10-17 16:17:41 -0700

    All the finished scripts. Should all be correct.

---
+++ Group Project/Assets/Scripts/FlyDeath.cs
@@ -0,0 +1,23 @@
+﻿/* By: Ryan Scheppler
+ * Date: 10/11/19
+ * Description: Add to fly enemy, handles fly specific death things
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyDeath : MonoBehaviour
+{
+    public float DeathDelay = 1;
+    Animator myAnimator;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        Instantiate(explosionPrefab, pos, rot);
+        Destroy(gameObject);
+    }
+}
